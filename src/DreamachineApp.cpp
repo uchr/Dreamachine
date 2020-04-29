455b0ad48216c698c9115f87e8dbdf9408f23518
Anton Tarasov <uchiro.me@gmail.com>
2020-04-29 21:23:19 +0700

    Added test triangle

--- src/DreamachineApp.cpp
+++ src/DreamachineApp.cpp
@@ -1,7 +1,12 @@
 #include <Corrade/Containers/Optional.h>
 
+#include <Magnum/GL/Buffer.h>
+#include <Magnum/GL/DefaultFramebuffer.h>
 #include <Magnum/GL/Framebuffer.h>
+#include <Magnum/GL/Mesh.h>
+#include <Magnum/Math/Color.h>
 #include <Magnum/Platform/GLContext.h>
+#include <Magnum/Shaders/VertexColor.h>
 
 /* If your application is using anything from QtGui, you might get warnings
    about GLEW and errors regarding GLclampf. If that's the case, uncomment the
@@ -27,17 +32,38 @@ class DreamachineApp: public QOpenGLWidget {
         void initializeGL() override;
         void paintGL() override;
 
-        Platform::GLContext& _context;
+        Platform::GLContext& m_context;
+        std::unique_ptr<GL::Mesh> m_mesh;
+        std::unique_ptr<Shaders::VertexColor2D> m_shader;
 };
 
-DreamachineApp::DreamachineApp(Platform::GLContext& context, QWidget* parent, Qt::WindowFlags f): QOpenGLWidget{parent, f}, _context(context) {
-    /* TODO: Add your context format setup code here */
+DreamachineApp::DreamachineApp(Platform::GLContext& context, QWidget* parent, Qt::WindowFlags f): QOpenGLWidget{parent, f}, m_context(context) {
 }
 
 void DreamachineApp::initializeGL() {
-    _context.create();
+    using namespace Math::Literals;
 
-    /* TODO: Add your initialization code here */
+    m_context.create();
+
+    struct TriangleVertex {
+        Vector2 position;
+        Color3 color;
+    };
+    const TriangleVertex vertexData[]{
+        {{-0.5f, -0.5f}, 0xff0000_rgbf},
+        {{ 0.5f, -0.5f}, 0x00ff00_rgbf},
+        {{ 0.0f,  0.5f}, 0x0000ff_rgbf}
+    };
+
+    GL::Buffer buffer;
+    buffer.setData(vertexData);
+
+    m_mesh = std::make_unique<GL::Mesh>();
+    m_mesh->setCount(3).addVertexBuffer(std::move(buffer), 0,
+                                       Shaders::VertexColor2D::Position{},
+                                       Shaders::VertexColor2D::Color3{});
+
+    m_shader = std::make_unique<Shaders::VertexColor2D>();
 
     /* Clean up Magnum state when giving control back to Qt */
     GL::Context::current().resetState(GL::Context::State::EnterExternal);
@@ -52,7 +78,7 @@ void DreamachineApp::paintGL() {
 
     qtDefaultFramebuffer.clear(GL::FramebufferClear::Color);
 
-    /* TODO: Add your drawing code here */
+    m_mesh->draw(*m_shader);
 
     /* Clean up Magnum state when giving control back to Qt */
     GL::Context::current().resetState(GL::Context::State::EnterExternal);
