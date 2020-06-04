bf4d9049207d5d4b5d4e3b8d1806ece4453df541
Anton Tarasov <uchiro.me@gmail.com>
2020-06-05 01:00:54 +0700

    Added parsing command line arguments

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 #include "Scene.h"
 #include "MeshExporter.h"
 
+#include <CLI/CLI.hpp>
 #include <spdlog/spdlog.h>
 
 #include <QSurfaceFormat>
 #include <QtWidgets/QApplication>
+
+#include <DirectXTex.h>
 
 #include <fstream>
 #include <cstdlib>
@@ -15,47 +18,74 @@
 using namespace parser;
 
 int main(int argc, char** argv) {
-    //spdlog::set_level(spdlog::level::debug);
-
     const char* dreamfallTLJResPath = std::getenv("DreamfallTLJResPath");
-
-    const std::string bundleName = "japan_streets";
-    const std::filesystem::path sceneSDRPath = "data/generated/locations/" + bundleName + ".cdr";
-
     PAKParser::instance() = PAKParser(dreamfallTLJResPath);
 
+    CLI::App cliapp{"Tool for extracting assets from Dreamfall: The Longest Journey"};
+
+    bool isDebugLog = false;
+    cliapp.add_flag("--debugLog", isDebugLog, "Enable debug log");
+    bool isExportMode = false;
+    cliapp.add_flag("--export", isExportMode, "Just export meshes without GUI");
+
+    std::string bundleName = "japan_streets";
+    cliapp.add_option("-p", bundleName, "Bundle name");
+
+    std::string meshName = "";
+    cliapp.add_option("-s", meshName, "Mesh name");
+
+    CLI11_PARSE(cliapp, argc, argv);
+
+    if (isDebugLog)
+        spdlog::set_level(spdlog::level::debug);
+
+    std::filesystem::path sceneSDRPath = "data/generated/locations/" + bundleName + ".cdr";
     SharkParser sceneSDRParser(sceneSDRPath);
     SceneIndex sceneIndex = sceneSDRParser.parseScene(bundleName);
 
-    Magnum::Platform::GLContext context{Magnum::NoCreate, argc, argv};
-    QApplication app{argc, argv};
-
-    QSurfaceFormat format;
-    format.setVersion(4, 6);
-    format.setProfile(QSurfaceFormat::CoreProfile);
-    QSurfaceFormat::setDefaultFormat(format);
-
-    MainWindow mw{context, sceneIndex};
-    mw.resize(1366, 768);
-    mw.show();
-
-    for (const auto& sir : sceneIndex.sirs) {
-        if (sir.filename.find("anim") == 0)
-            continue;
-
-        Scene scene(sir, bundleName);
-
-        if (scene.sceneRoot.has_value())
-            spdlog::info("SIR: '{}' parsed", sir.filename);
-        else 
-            spdlog::warn("SIR: '{}' not parsed", sir.filename);
-
-        if (scene.sceneRoot.has_value()) {
-            auto isExtracted = exportScene(*scene.sceneRoot, bundleName, sir.filename);
-            if (!isExtracted)
-                spdlog::error("Scene: '{}' not extracted", sir.filename);
+    if (isExportMode) {
+        HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+        if (FAILED(hr)) {
+            spdlog::critical("DirectXTex failed initialization");
+            return 1;
         }
-    }
 
-    return app.exec();
+        for (const auto& sir : sceneIndex.sirs) {
+            if (sir.filename.find("anim") == 0)
+                continue;
+
+            if (!meshName.empty() && sir.filename.find(meshName) == std::string::npos)
+                continue;
+
+            Scene scene(sir, bundleName);
+
+            if (scene.sceneRoot.has_value())
+                spdlog::info("SIR: '{}' parsed", sir.filename);
+            else 
+                spdlog::warn("SIR: '{}' not parsed", sir.filename);
+
+            if (scene.sceneRoot.has_value()) {
+                auto isExtracted = exportScene(*scene.sceneRoot, bundleName, sir.filename);
+                if (!isExtracted)
+                    spdlog::error("Scene: '{}' not extracted", sir.filename);
+            }
+        }
+
+        return 0;
+    }
+    else {
+        Magnum::Platform::GLContext context{Magnum::NoCreate, argc, argv};
+        QApplication app{argc, argv};
+
+        QSurfaceFormat format;
+        format.setVersion(4, 6);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+        QSurfaceFormat::setDefaultFormat(format);
+
+        MainWindow mw{context, sceneIndex};
+        mw.resize(1366, 768);
+        mw.show();
+
+        return app.exec();
+    }
 }
